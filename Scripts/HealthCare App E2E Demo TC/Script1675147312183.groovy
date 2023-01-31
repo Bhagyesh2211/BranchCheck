21af884d41f0c00e698d5b13c9408a003295f2dc
bhagyesh <bhagyesh@bhagyeshs-mbp.bbrouter>
2023-01-31 15:42:50 +0530

    Initial Commit

--- Scripts/HealthCare App E2E Demo TC/Script1675147312183.groovy
+++ Scripts/HealthCare App E2E Demo TC/Script1675147312183.groovy
@@ -17,19 +17,21 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
-
-WebUI.click(findTestObject('Object Repository/HealthCare App E2E Demo TO/Page_CURA Healthcare Service/a_Make Appointment'))
-
-WebUI.setText(findTestObject('Object Repository/HealthCare App E2E Demo TO/Page_CURA Healthcare Service/input_Username_username'), 
-    'John Doe')
-
-WebUI.setEncryptedText(findTestObject('Object Repository/HealthCare App E2E Demo TO/Page_CURA Healthcare Service/input_Password_password'), 
-    'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
-
-WebUI.click(findTestObject('Object Repository/HealthCare App E2E Demo TO/Page_CURA Healthcare Service/button_Login'))
+//WebUI.openBrowser('')
+//
+//'Navigating to the URL'
+//WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+//
+//WebUI.click(findTestObject('Object Repository/HealthCare App E2E Demo TO/Page_CURA Healthcare Service/a_Make Appointment'))
+//
+//WebUI.setText(findTestObject('Object Repository/HealthCare App E2E Demo TO/Page_CURA Healthcare Service/input_Username_username'), 
+//    'John Doe')
+//
+//WebUI.setEncryptedText(findTestObject('Object Repository/HealthCare App E2E Demo TO/Page_CURA Healthcare Service/input_Password_password'), 
+//    'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
+//
+//WebUI.click(findTestObject('Object Repository/HealthCare App E2E Demo TO/Page_CURA Healthcare Service/button_Login'))
+CustomKeywords.'com.kms.katalon.keyword.applitools.BasicKeywords.checkElement'(null, null)
 
 WebUI.click(findTestObject('Object Repository/HealthCare App E2E Demo TO/Page_CURA Healthcare Service/input_Apply for hospital readmission_hospital_readmission'))
 
